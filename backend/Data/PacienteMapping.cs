ed77553d34718f3d682c27c66ce53b87ad6c7c3f
vitorhenrique-ufg <vitorhfbc2@hotmail.com>
2023-02-25 21:20:40 -0300

    Remove arquivos do gitignore

---
+++ backend/Data/PacienteMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PAS.Domain;
+
+namespace PAS.Data
+{
+    public class PacienteMapping : IEntityTypeConfiguration<Paciente>
+    {
+        public PacienteMapping()
+        {
+        }
+
+        public void Configure(EntityTypeBuilder<Paciente> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.ToTable("Paciente", "Sistema");
+        }
+    }
+}
---
+++ backend/Data/Seeder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PAS.Data
+{
+    public class Seeder
+    {
+        private readonly DbContext _dbContext;
+
+
+        public Seeder(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Migrate()
+        {
+            _dbContext.Database.Migrate();
+        }
+    }
+}
